52955ce220aadb2c12adc3bfc2e0ff4819d8fba0
lrsgzs <99574908+lrsgzs@users.noreply.github.com>
2022-04-05 22:55:36 +0800

    Update __init__.py

--- lrslib/__init__.py
+++ lrslib/__init__.py
@@ -15,7 +15,7 @@ def func(fun='all'):
                 'all':'''全部函数：
 'tkin.Window':'仿tk窗口，用Button时，Tk()为 .win',
 'tools.pront':'一个一个地打印文本',
-'tools.show_image':'显示图片在pygame窗口中'
+'tools.show_image':'显示图片在pygame窗口中',
 'by':'关于'
 'func':'帮助'
 '''
